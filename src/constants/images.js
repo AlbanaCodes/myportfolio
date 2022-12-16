9fd8ac5fddc63ef052a18ac5ceda239b725d9f00
Albana <28781490+AlbanaCodes@users.noreply.github.com>
2022-12-16 21:58:27 +0100

    add more assets

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,5 +1,24 @@
 import logo from '../assets/logo.png';
+// import profile from '../assets/profile.png';
+import bgIMG from '../assets/bgIMG.png';
+import bgWhite from '../assets/bgWhite.png';
+import css from '../assets/css.png';
+import email from '../assets/email.png';
+import git from '../assets/git.png';
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import react from '../assets/react.png';
+import sass from '../assets/sass.png';
 
 export default {
-	logo
+	logo,
+	bgIMG,
+	bgWhite,
+	css,
+	email,
+	git,
+	html,
+	javascript,
+	react,
+	sass
 };
