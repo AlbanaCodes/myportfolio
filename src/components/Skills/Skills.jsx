a6f4a3d8c33b078378e71c962b797aaa802ae06c
Albana <28781490+AlbanaCodes@users.noreply.github.com>
2022-12-24 23:38:49 +0100

    fix key uniquenes and react tooltip

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { Tooltip as ReactTooltip } from 'react-tooltip'
+import { Tooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css'
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Skills.scss';
@@ -14,68 +15,68 @@ const Skills = () => {
 		const skillsQuery = '*[_type == "skills"]';
 
 		client.fetch(query).then((data) => {
+			console.log('exp: ', data);
 			setExperiences(data);
 		});
 
 		client.fetch(skillsQuery).then((data) => {
 			setSkills(data);
 		});
-}, []);
+	}, []);
 
   return (
   <>
 	<h2 className="head-text">Skills & Experiences</h2>
 
     <div className="app__skills-container">
-    <motion.div className="app__skills-list">
-    {
-		skills.map((skill) => (
-        	<motion.div whileInView={{ opacity: [0, 1] }}
-            		    transition={{ duration: 0.5 }}
-              			className="app__skills-item app__flex"
-              			key={skill.name} >
-                <div className="app__flex"
-            	 	style={{ backgroundColor: skill.bgColor }} >
-                	<img src={urlFor(skill.icon)} alt={skill.name} />
-            	</div>
-              	<p className="p-text">{skill.name}</p>
-            </motion.div>
-        ))
-	}
-    </motion.div>
+		<motion.div className="app__skills-list">
+		{
+			skills.map((skill, index) => (
+				<motion.div whileInView={{ opacity: [0, 1] }}
+							transition={{ duration: 0.5 }}
+							className="app__skills-item app__flex"
+							key={`skill-${skill.name}-${index}`} >
+					<div className="app__flex"
+						style={{ backgroundColor: skill.bgColor }} >
+						<img src={urlFor(skill.icon)} alt={skill.name} />
+					</div>
+					<p className="p-text">{skill.name}</p>
+				</motion.div>
+			))
+		}
+		</motion.div>
         <div className="app__skills-exp">
 		{
-			experiences.map((experience) => (
-            <motion.div className="app__skills-exp-item"
-              			key={experience.year} >
-              	<div className="app__skills-exp-year">
-                	<p className="bold-text">{experience.year}</p>
-              	</div>
-              	<motion.div className="app__skills-exp-works">
-                {
-					experience.works.map((work) => (
-                    <>
-						<motion.div whileInView={{ opacity: [0, 1] }}
-									transition={{ duration: 0.5 }}
-									className="app__skills-exp-work"
-									data-tip
-									data-for={work.name}
-									key={work.name} >
-							<h4 className="bold-text">{work.name}</h4>
-							<p className="p-text">{work.company}</p>
-						</motion.div>
-						<ReactTooltip id={work.name}
-									  effect="solid"
-									  arrowColor="#fff"
-									  className="skills-tooltip" >
-							{work.desc}
-						</ReactTooltip>
-                    </>
-                	))
-				}
-              	</motion.div>
-            </motion.div>
-          ))
+			experiences.map((experience, index) => (
+				<motion.div className="app__skills-exp-item"
+							key={`experience-${experience.year}-${index}`} >
+					<div className="app__skills-exp-year">
+						<p className="bold-text">{experience.year}</p>
+					</div>
+					<motion.div className="app__skills-exp-works">
+					{
+						experience.works.map((work, index) => (
+						<div key={`work-${work.name}-${work.company}-${index}`}>
+							<motion.div whileInView={{ opacity: [0, 1] }}
+										transition={{ duration: 0.5 }}
+										className="app__skills-exp-work"
+										data-tip
+										data-for={work.name}
+										id={`${work.name}-${work.company}`} >
+								<h4 className="bold-text">{work.name}</h4>
+								<p className="p-text">{work.company}</p>
+							</motion.div>
+
+							<Tooltip anchorId={`${work.name}-${work.company}`} 
+									content={work.desc} 
+									place="top"
+									className="skills-tooltip" />
+						</div>
+						))
+					}
+					</motion.div>
+				</motion.div>
+          	))
 		}
         </div>
     </div>
