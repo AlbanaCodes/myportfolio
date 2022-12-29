d6e96024b5bd8be103c0d376606be3c53718012b
Albana <28781490+AlbanaCodes@users.noreply.github.com>
2022-12-29 09:41:48 +0100

    link social media accounts

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -5,10 +5,14 @@ const SocialMedia = () => {
 	return (
 		<div className="app__social">
 			<div>
-				<BsGithub />
+				<a href="https://github.com/AlbanaCodes" target="_blank">
+					<BsGithub />
+				</a>
 			</div>
 			<div>
-				<BsLinkedin />
+				<a href="https://www.linkedin.com/in/albane-krasniqi-4b00a4257/" target="_blank">
+					<BsLinkedin />
+				</a>
 			</div>
 		</div>
   	)
